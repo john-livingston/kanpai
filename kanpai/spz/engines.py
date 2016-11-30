7c35da8e2aac1f4a5f5d7f81d0218198e8b6f244
john-livingston <jliv84@gmail.com>
2016-12-01 00:05:24 +0900

    moved MAP and MCMC to separate module, improving result aggregation

---
+++ kanpai/spz/engines.py
@@ -0,0 +1,204 @@
+import os
+import numpy as np
+import scipy.optimize as op
+from emcee import MHSampler, EnsembleSampler
+from emcee.utils import sample_ball
+from tqdm import tqdm
+
+import util
+import plot
+
+
+class MAP(object):
+
+    def __init__(self, logprob, ini, args, methods=('nelder-mead', 'powell')):
+
+        """
+        Maximum a posteriori model fit. Defaults to Nelder-Mead and Powell.
+
+        :param logprob  : log-probability (posterior) function to maximize
+        :param ini      : initial parameter vector
+        :param args     : any additional args to be passed to logprob
+        :param methods  : list of methods to be used from scipy.optimize
+        """
+
+        self._logprob = logprob
+        self._ini = ini
+        self._args = args
+        self._methods = methods
+
+    def _map(self, method='nelder-mead'):
+
+        nlp = lambda *x: -self._logprob(*x)
+        initial = self._ini
+        args = self._args
+        res = op.minimize(nlp, initial, args=args, method=method)
+
+        return res
+
+    def run(self):
+
+        print "\nAttempting maximum a posteriori optimization"
+        results = []
+        for method in self._methods:
+            res = self._map(method=method)
+            if res.success:
+                print "Log probability ({}): {}".format(method, -res.fun)
+                results.append(res)
+
+        if len(results) > 0:
+            idx = np.argmin([r.fun for r in results])
+            map_best = np.array(results)[idx]
+            lp_map = -1 * map_best.fun
+            pv_map = map_best.x
+            lp_ini = self._logprob(self._ini, *self._args)
+            if lp_map > lp_ini:
+                method = np.array(self._methods)[idx]
+                self._pv, self._lp, self._method = pv_map, lp_map, method
+            else:
+                self._pv, self._lp, self._method = self._ini, lp_ini, 'initial'
+        else:
+            print "All methods failed to converge"
+
+        self._hasrun = True
+
+    @property
+    def results(self):
+
+        if not self._hasrun:
+            print "Need to call run() first!"
+
+        return self._pv, self._lp, self._method
+
+
+class MCMC(object):
+
+    def __init__(self, logprob, ini, args, names, outdir=None):
+
+        """
+        Affine invariant ensemble MCMC exploration of parameter space.
+
+        :param logprob  : log-probability (posterior) function to maximize
+        :param ini      : initial parameter vector
+        :param args     : any additional args to be passed to logprob
+        :param names    : list of parameter names
+        :param outdir   : path to output directory
+        """
+
+        self._logprob = logprob
+        self._ini = ini
+        self._args = args
+        self._names = names
+        self._outdir = outdir
+        self._logprob_ini = logprob(ini, *args)
+
+    def run(self, nproc=4, nsteps1=1e3, nsteps2=1e3, max_steps=1e4, gr_threshold=1.1, pos_idx=None, save=True, make_plots=True):
+
+        """
+        :param nproc        : number of processes to use for sampling
+        :param nsteps1      : number of steps to take during stage 1 exploration
+        :param nsteps2      : number of steps to take during each stage 2 iteration
+        :param max_steps    : maximum number of steps to take during stage 2
+        :param gr_threshold : Gelman-Rubin convergence threshold
+        :param pos_idx      : indices of any parameters that should always be positive
+        :param save         : whether or not to save MCMC samples and related output
+        :param plot         : whether or not to generate plots
+        """
+
+        logprob = self._logprob
+        pv_ini = self._ini
+        args = self._args
+        names = self._names
+        logprob_ini = self._logprob_ini
+
+        if save or plot:
+            assert self._outdir is not None
+
+        ndim = len(pv_ini)
+        nwalkers = 8 * ndim if ndim > 12 else 16 * ndim
+        print "\nRunning MCMC"
+        print "{} walkers exploring {} dimensions".format(nwalkers, ndim)
+
+        sampler = EnsembleSampler(nwalkers, ndim, logprob,
+            args=args, threads=nproc)
+        pos0 = sample_ball(pv_ini, [1e-4]*ndim, nwalkers) # FIXME use individual sigmas
+        if pos_idx is not None:
+            pos0[pos_idx] = np.abs(pos0[pos_idx])
+
+        print "\nstage 1"
+        for pos,_,_ in tqdm(sampler.sample(pos0, iterations=nsteps1)):
+            pass
+
+        if make_plots:
+            fp = os.path.join(self._outdir, 'chain-initial.png')
+            plot.chain(sampler.chain, names, fp)
+
+        idx = np.argmax(sampler.lnprobability)
+        new_best = sampler.flatchain[idx]
+        new_prob = sampler.lnprobability.flat[idx]
+        best = new_best if new_prob > logprob_ini else pv_ini
+        pos = sample_ball(best, [1e-6]*ndim, nwalkers) # FIXME use individual sigmas
+        if pos_idx is not None:
+            pos0[pos_idx] = np.abs(pos0[pos_idx])
+        sampler.reset()
+
+        print "\nstage 2"
+        nsteps = 0
+        gr_vals = []
+        while nsteps < max_steps:
+            for pos,_,_ in tqdm(sampler.sample(pos, iterations=nsteps2)):
+                pass
+            nsteps += nsteps2
+            gr = util.gelman_rubin(sampler.chain)
+            gr_vals.append(gr)
+            msg = "After {} steps\n  Mean G-R: {}\n  Max G-R: {}"
+            print msg.format(nsteps, gr.mean(), gr.max())
+            if (gr < gr_threshold).all():
+                break
+
+        idx = gr_vals[-1] >= gr_threshold
+        if idx.any():
+            print "WARNING -- some parameters failed to converge below threshold:"
+            print np.array(names)[idx]
+
+        self._lp = sampler.lnprobability.flatten().max()
+        idx = np.argmax(sampler.lnprobability)
+        assert sampler.lnprobability.flat[idx] == self._lp
+        self._pv = sampler.flatchain[idx]
+
+        burn = nsteps - nsteps2 if nsteps > nsteps2 else 0
+        thin = 1
+        self._fc = sampler.chain[:,burn::thin,:].reshape(-1, ndim)
+
+        if make_plots:
+            fp = os.path.join(self._outdir, 'gr.png')
+            plot.gr_iter(gr_vals, fp)
+
+            fp = os.path.join(self._outdir, 'chain.png')
+            plot.chain(sampler.chain, names, fp)
+
+            fp = os.path.join(self._outdir, 'corner.png')
+            plot.corner(self._fc, names, fp=fp, truths=self._pv)
+
+        if save:
+            fp = os.path.join(self._outdir, 'mcmc')
+            np.savez_compressed(
+                fp,
+                flat_chain=self._fc,
+                logprob_best=self._lp,
+                pv_best=self._pv,
+                pv_names=names,
+                gelman_rubin=np.array(gr_vals)
+                )
+
+        self._gr = gr_vals[-1]
+        self._acor = sampler.acor
+        self._hasrun = True
+
+    @property
+    def results(self):
+
+        if not self._hasrun:
+            print "Need to call run() first!"
+
+        return self._pv, self._lp, self._fc, self._gr, self._acor
--- kanpai/util.py
+++ kanpai/util.py
@@ -1,12 +1,28 @@
 import os
 import yaml
+import numpy as np
 import pandas as pd
+import astropy.units as u
+
+import forecaster
+
+SOLRAD = (u.Rsun / u.Rearth).to(u.dimensionless_unscaled)
+
+def geom_mean(x):
+    x = np.abs(x)
+    gm = np.sqrt(np.product(x)) if x.size > 1 else x
+    return gm
 
 def aggregate_results(list_of_output_dirs):
 
     starids,planets,rs,urs,tf,utf = [],[],[],[],[],[]
-    k_k2,k_k2_plus,k_k2_minus = [],[],[]
-    k_sp,k_sp_plus,k_sp_minus = [],[],[]
+
+    ars,uars_plus,uars_minus = [],[],[]
+    inc,uinc_plus,uinc_minus = [],[],[]
+    k_k2,uk_k2_plus,uk_k2_minus = [],[],[]
+    k_sp,uk_sp_plus,uk_sp_minus = [],[],[]
+    tc_sp,utc_sp_plus,utc_sp_minus = [],[],[]
+
 
     for d in list_of_output_dirs:
 
@@ -28,15 +44,31 @@ def aggregate_results(list_of_output_dirs):
             bic = outfile['spz']['bic']
             rchisq = outfile['spz']['reduced_chisq']
 
+            a,b,c = outfile['percentiles']['a']
+            ars.append(b)
+            uars_plus.append(c-b)
+            uars_minus.append(b-a)
+
+            a,b,c = outfile['percentiles']['i']
+            inc.append(b * 180/np.pi)
+            uinc_plus.append((c-b) * 180/np.pi)
+            uinc_minus.append((b-a) * 180/np.pi)
+
             a,b,c = outfile['percentiles']['k_k']
             k_k2.append(b)
-            k_k2_plus.append(c-b)
-            k_k2_minus.append(b-a)
+            uk_k2_plus.append(c-b)
+            uk_k2_minus.append(b-a)
 
             a,b,c = outfile['percentiles']['k_s']
             k_sp.append(b)
-            k_sp_plus.append(c-b)
-            k_sp_minus.append(b-a)
+            uk_sp_plus.append(c-b)
+            uk_sp_minus.append(b-a)
+
+            a,b,c = outfile['percentiles']['tc_s']
+            tc_sp.append(b)
+            utc_sp_plus.append(c-b)
+            utc_sp_minus.append(b-a)
+
         except KeyError as e:
             print fp, e
 
@@ -48,20 +80,78 @@ def aggregate_results(list_of_output_dirs):
             urstar=urs,
             teff=tf,
             uteff=utf,
+            ars=ars,
+            uars_plus=uars_plus,
+            uars_minus=uars_minus,
+            inc=inc,
+            uinc_plus=uinc_plus,
+            uinc_minus=uinc_minus,
             k_k2=k_k2,
-            k_k2_plus=k_k2_plus,
-            k_k2_minus=k_k2_minus,
+            uk_k2_plus=uk_k2_plus,
+            uk_k2_minus=uk_k2_minus,
             k_sp=k_sp,
-            k_sp_plus=k_sp_plus,
-            k_sp_minus=k_sp_minus,
+            uk_sp_plus=uk_sp_plus,
+            uk_sp_minus=uk_sp_minus,
+            tc_sp=tc_sp,
+            utc_sp_plus=utc_sp_plus,
+            utc_sp_minus=utc_sp_minus,
             )
         )
 
     return df
 
 
-def make_table(aggregated_results):
-    raise NotImplementedError
+def add_mass(df):
 
-def save_to_latex(table, fp):
-    raise NotImplementedError
+    df['re_sp'] = df['k_sp'] * df['rstar'] * SOLRAD
+    df['re_k2'] = df['k_k2'] * df['rstar'] * SOLRAD
+
+    df['uk_k2'] = df[['k_k2_plus', 'k_k2_minus']].apply(geom_mean, axis=1, raw=True)
+    df['uk_sp'] = df[['k_sp_plus', 'k_sp_minus']].apply(geom_mean, axis=1, raw=True)
+
+    urstar_frac = df['urstar'] / df['rstar']
+    uk_k2_frac = df['uk_k2'] / df['k_k2']
+    uk_sp_frac = df['uk_sp'] / df['k_sp']
+
+    df['ure_k2'] = df['re_k2'] * (urstar_frac + uk_k2_frac)
+    df['ure_sp'] = df['re_sp'] * (urstar_frac + uk_sp_frac)
+
+    mass = [mr.Rstat2M(re, ure, unit='Earth') for re,ure in zip(df['re_k2'], df['ure_k2'])]
+    umass2 = [k[1:] if k is not None else np.nan for k in mass]
+    umass1 = [geom_mean(i) if np.isfinite(i).all() else np.nan for i in umass2]
+    mass1 = [k[0] if k is not None else np.nan for k in mass]
+
+    df['me_k2'] = mass1
+    df['ume_k2'] = umass1
+    df['ume_k2_2'] = umass2
+
+
+def make_table1(df):
+    cols = 'starid k_k2 uk_k2_plus uk_k2_minus k_sp uk_sp_plus uk_sp_minus'.split()
+    lines = []
+    for idx in df.index:
+        vals = df.loc[idx][cols].tolist()
+        text = '{0} & ${1:.4f}^(+{2:.4f})_(-{3:.4f})$ & ${4:.4f}^(+{5:.4f})_(-{6:.4f})$\\\\'
+        line = text.format(*vals).replace('(','{').replace(')','}')
+        lines.append(line)
+    return '\n'.join(lines)
+
+
+def make_table2(df):
+    cols = 'starid ars uars_plus uars_minus inc uinc_plus uinc_minus'
+    cols += ' k_k2 uk_k2_plus uk_k2_minus k_sp uk_sp_plus uk_sp_minus'
+    cols += ' tc_sp utc_sp_plus utc_sp_minus'
+    cols = cols.split()
+    lines = []
+    for idx in df.index:
+        vals = df.loc[idx][cols].tolist()
+        text = '{0} & ${1:.4f}^(+{2:.4f})_(-{3:.4f})$ & ${4:.4f}^(+{5:.4f})_(-{6:.4f})$'
+        text += ' & ${7:.4f}^(+{8:.4f})_(-{9:.4f})$ & ${10:.4f}^(+{11:.4f})_(-{12:.4f})$'
+        text += ' & ${13:.4f}^(+{14:.4f})_(-{15:.4f})$\\\\'
+        line = text.format(*vals).replace('(','{').replace(')','}')
+        lines.append(line)
+    return '\n'.join(lines)
+
+
+def save_to_latex(df, fp):
+    df.to_latex(open('test_table.tex', 'w'))
