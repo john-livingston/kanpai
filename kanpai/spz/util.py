6a419ed5ec0c7199843ef8de2a7eefabb0a0c9c5
John Livingston <jliv84@gmail.com>
2017-02-17 18:51:36 +0900

    bugfix

--- kanpai/spz/util.py
+++ kanpai/spz/util.py
@@ -6,9 +6,9 @@ from .. import util
 def parse_setup(fp):
     setup = yaml.load(open(fp))
     transit = setup['transit']
-    if 'b' not in transit.keys():
+    if 'b' not in transit.keys() or transit['b'] is None:
         transit['b'] = 0
-    if 'i' not in transit.keys():
+    if 'i' not in transit.keys() or transit['i'] is None:
         transit['i'] = util.transit.inclination(a, transit['b'])
     if 't14' not in transit.keys():
         try:
@@ -17,7 +17,7 @@ def parse_setup(fp):
         except KeyError as e:
             msg = "{} is missing! unable to compute transit duration"
             print(msg.format(e))
-    if 'a' not in transit.keys():
+    if 'a' not in transit.keys() or transit['a'] is None:
         try:
             p = transit['p']
             t14 = transit['t14']
