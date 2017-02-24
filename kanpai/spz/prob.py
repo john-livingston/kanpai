0c57a18975851803c2b5d267f8405d342177c71a
john-livingston <jliv84@gmail.com>
2017-02-24 13:48:32 +0900

    added spz tc prior

--- kanpai/spz/prob.py
+++ kanpai/spz/prob.py
@@ -17,7 +17,8 @@ def logprob_u(theta, t, f, p, aux=None, ldp=None, ret_pvnames=False, ret_mod=Fal
 
     k,tc,a,b,u1,u2,s,k1 = theta[:8]
 
-    if u1 < 0 or u1 > 2 or u2 < -1 or u2 > 1 or b < 0 or b > 1+k:
+    if u1 < 0 or u1 > 2 or u2 < -1 or u2 > 1 or b < 0 or b > 1+k or \
+        tc < t[0] - 0.05 or tc > t[-1] + 0.05:
         return -np.inf
 
     lp = 0
@@ -44,7 +45,8 @@ def logprob_q(theta, t, f, p, aux=None, ldp=None, ret_pvnames=False, ret_mod=Fal
 
     k,tc,a,b,q1,q2,s,k1 = theta[:8]
 
-    if q1 < 0 or q1 > 1 or q2 < 0 or q2 > 1 or b < 0 or b > 1+k:
+    if q1 < 0 or q1 > 1 or q2 < 0 or q2 > 1 or b < 0 or b > 1+k or \
+        tc < t[0] - 0.05 or tc > t[-1] + 0.05:
         return -np.inf
 
     u1, u2 = util.ld.q_to_u(q1, q2)
