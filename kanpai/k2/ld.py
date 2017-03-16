d3a6c69ef93a2816a0818998a082a82889de645c
John Livingston <jliv84@gmail.com>
2017-03-17 04:53:47 +0900

    upgrade limb darkening

--- kanpai/k2/ld.py
+++ kanpai/k2/ld.py
@@ -8,13 +8,13 @@ import limbdark
 import band
 
 
-def get_ld_claret(teff, uteff, logg, ulogg, band='Kp'):
+def claret(teff, uteff, logg, ulogg, feh, ufeh, band='Kp'):
 
     mult = 1
     u = np.repeat(np.nan, 4)
     while np.isnan(u).any():
+        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
         mult += 1
-        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg)
 
     u = u.tolist()
     # boost uncertainties by factor of 2
@@ -23,7 +23,7 @@ def get_ld_claret(teff, uteff, logg, ulogg, band='Kp'):
 
     print "{0} u1: {1:.4f} +/- {2:.4f}, u2: {3:.4f} +/- {4:.4f}".format(band, *u)
 
-    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg)
+    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
     print "Using {} models".format(df.shape[0])
     for key in "teff logg feh".split():
         print "{} range: {} - {}".format(key, df[key].min(), df[key].max())
--- kanpai/spz/ld.py
+++ kanpai/spz/ld.py
@@ -1,18 +1,15 @@
 import numpy as np
 
 import limbdark
-from ldtk import LDPSetCreator, BoxcarFilter, TabulatedFilter
-
-from ..k2 import band as k2_band
 
 
-def get_ld_claret(teff, uteff, logg, ulogg, band='S2'):
+def claret(teff, uteff, logg, ulogg, feh, ufeh, band='S2'):
 
     mult = 1
     u = np.repeat(np.nan, 4)
     while np.isnan(u).any():
+        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
         mult += 1
-        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg)
 
     u = u.tolist()
     # boost uncertainties by factor of 2
@@ -21,7 +18,7 @@ def get_ld_claret(teff, uteff, logg, ulogg, band='S2'):
 
     print "{0} u1: {1:.4f} +/- {2:.4f}, u2: {3:.4f} +/- {4:.4f}".format(band, *u)
 
-    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg)
+    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
     print "Using {} models".format(df.shape[0])
     for key in "teff logg feh".split():
         print "{} range: {} - {}".format(key, df[key].min(), df[key].max())
