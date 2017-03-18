d422b4655d2ee7ecedd571a68cd09a9326ef1caf
John Livingston <jliv84@gmail.com>
2017-03-18 23:48:00 +0900

    cleanup

--- kanpai/k2/ld.py
+++ kanpai/k2/ld.py
@@ -1,37 +1,18 @@
+import functools
 import itertools
 import numpy as np
 import statsmodels.api as sm
 
 from ldtk import LDPSetCreator, BoxcarFilter, TabulatedFilter
-import limbdark
 
+from .. import util
 import band
 
 
-def claret(teff, uteff, logg, ulogg, feh, ufeh, band='Kp'):
-
-    mult = 1
-    u = np.repeat(np.nan, 4)
-    while np.isnan(u).any():
-        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
-        mult += 1
-
-    u = u.tolist()
-    # boost uncertainties by factor of 2
-    u[1] *= 2
-    u[3] *= 2
-
-    print "{0} u1: {1:.4f} +/- {2:.4f}, u2: {3:.4f} +/- {4:.4f}".format(band, *u)
-
-    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
-    print "Using {} models".format(df.shape[0])
-    for key in "teff logg feh".split():
-        print "{} range: {} - {}".format(key, df[key].min(), df[key].max())
-
-    return u
+claret = functools.partial(util.ld.claret, band='Kp')
 
 
-def get_ld_ldtk(teff, uteff, logg, ulogg, feh, ufeh, kind='quadratic'):
+def ldtk(teff, uteff, logg, ulogg, feh, ufeh, kind='quadratic'):
 
     filters = [TabulatedFilter('Kepler', band.lam, band.tra)]
     sc = LDPSetCreator(teff=(teff,uteff), logg=(logg,ulogg), z=(feh,ufeh), filters=filters)
--- kanpai/util/ld.py
+++ kanpai/util/ld.py
@@ -12,3 +12,27 @@ def u_to_q(u1, u2):
     q1 = (u1 + u2)**2
     q2 = u1 / (2 * (u1 + u2))
     return q1, q2
+
+
+
+def claret(teff, uteff, logg, ulogg, feh, ufeh, band):
+
+    mult = 1
+    u = np.repeat(np.nan, 4)
+    while np.isnan(u).any():
+        u[:] = limbdark.get_ld(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
+        mult += 1
+
+    u = u.tolist()
+    # boost uncertainties by factor of 2
+    u[1] *= 2
+    u[3] *= 2
+
+    print "{0} u1: {1:.4f} +/- {2:.4f}, u2: {3:.4f} +/- {4:.4f}".format(band, *u)
+
+    df = limbdark.get_ld_df(band, teff, mult * uteff, logg, mult * ulogg, feh, mult * ufeh)
+    print "Using {} models".format(df.shape[0])
+    for key in "teff logg feh".split():
+        print "{} range: {} - {}".format(key, df[key].min(), df[key].max())
+
+    return u
