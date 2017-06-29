2d5d04bf8e0dbf78203b3ef162e06ce2a1c89e06
john-livingston <jliv84@gmail.com>
2017-06-30 07:43:04 +0900

    adjusting prior

--- kanpai/gen/prob.py
+++ kanpai/gen/prob.py
@@ -51,21 +51,21 @@ def logprob_gp(theta, t, f, p, aux=None, ldp=None, ret_pvnames=False, ret_mod=Fa
 
     if not 0 < k < 1:
         return -np.inf
-    if not b < a:
+    if not b <= a:
         return -np.inf
     if not 0 < q1 < 1:
         return -np.inf
     if not 0 < q2 < 1:
         return -np.inf
-    if not 0 < b < 1+k:
+    if not 0 <= b < 1+k:
         return -np.inf
     if not t[0]-0.05 < tc < t[-1]+0.05:
         return -np.inf
-    if not -5 < lna < 5:
+    if not -10 < lna < 10:
         return -np.inf
-    if not -5 < lntau < 5:
+    if not -10 < lntau < 10:
         return -np.inf
-    if not -20 < ls < 5:
+    if not -10 < ls < 10:
         return -np.inf
 
     u1, u2 = util.ld.q_to_u(q1, q2)
